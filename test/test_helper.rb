4c7b123ff770deff3ca3a686236377dd0c5d0489
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-03-17 08:03:53 +0000

    add-Miniteset_Reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
+  # Run tests in parallel with specified workers（指定のワーカー数でテストを並列実行する）
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
