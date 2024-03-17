345036a6ed2de83cfbba606b8082bef4f3c96c11
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-03-17 07:45:30 +0000

    Completed ch03

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # GET / => static_pages#home
+  root 'static_pages#home'
+
+  # GET / => static_pages#home => static_pages#home
+  # GET / => about => static_pages#about
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'hello#index'
 end
