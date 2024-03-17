1aac46bc57e4fb881a28ae0a61f5b779ad4f7790
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-03-17 03:17:07 +0000

    improve-static_pages_controller-test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,20 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
+    # GET /static_pages/home
     get static_pages_home_url
     assert_response :success
   end
 
   test "should get help" do
+    # GET /static_pages/help
     get static_pages_help_url
     assert_response :success
   end
+
+  # test "should get about" do
+  #   # GET /static_pages/about
+  #   get static_pages_about_url
+  #   assert_response :success
+  # end
 end
