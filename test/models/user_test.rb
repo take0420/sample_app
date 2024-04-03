617802297610e9f959e50a8c593ed72f2fda769b
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-04-03 00:58:15 +0000

    add-models-length-test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@exaaaample.com")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "should be valid" do
@@ -11,12 +11,22 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-    @user.name =" "
+    @user.name = ""
     assert_not @user.valid?
   end
 
   test "email should be present" do
-    @user.email =" "
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
 end
