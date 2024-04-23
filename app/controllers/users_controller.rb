0aa3db2d11170bfbf6855a1026ca55367b206bad
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-04-23 12:14:40 +0000

    Add display of error messages on user registration failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,9 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    # Strong Parameters
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                   :password_confirmation)
+    end
 end
