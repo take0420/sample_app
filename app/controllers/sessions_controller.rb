52184285ede6fbd0112a68fb5f791ec3e7956a67
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-04-28 03:57:44 +0000

    add login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    # × @session = Session.new
+    # ◯ scope: :session + url: login_path
+  end
+
+  # POST /login
+  def create
+    user = User.find_by(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # Success
+      # login(@user)
+    else
+      # Failure
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
   end
 end
