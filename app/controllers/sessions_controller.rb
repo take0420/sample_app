9ae0df736534b967306e6537e51a26f2b0359948
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-04-29 04:57:10 +0000

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,15 @@
 class SessionsController < ApplicationController
+
   def new
-    # × @session = Session.new
-    # ◯ scope: :session + url: login_path
   end
 
-  # POST /login
   def create
-    user = User.find_by(params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Success
-      # login(@user)
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      # Failure
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
   end
