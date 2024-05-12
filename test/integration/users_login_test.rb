bcde2522ccc199b179b0f9cb5e150fca158ba780
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-05-12 03:25:57 +0000

    Refactored login with valid email/invalid password

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,22 +7,22 @@ class UsersLogin < ActionDispatch::IntegrationTest
   end
 end
 
-# class InvalidPasswordTest < UsersLogin
+class InvalidPasswordTest < UsersLogin
 
-#   test "login path" do
-#     get login_path
-#     assert_template 'sessions/new'
-#   end
+  test "login path" do
+    get login_path
+    assert_template 'sessions/new'
+  end
 
-#   test "login with valid email/invalid password" do
-#     post login_path, params: { session: { email: @user.email, password: "invalid" } }
-#     assert_not is_logged_in?
-#     assert_template 'sessions/new'
-#     assert_not flash.empty?
-#     get root_path
-#     assert flash.empty?
-#   end
-# end
+  test "login with valid email/invalid password" do
+    post login_path, params: { session: { email: @user.email, password: "invalid" } }
+    assert_not is_logged_in?
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end
 
 # class ValidLogin < UsersLogin
 
