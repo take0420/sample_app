f6f3451924f93397b4e7dc365c51330e3870cef0
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-05-23 00:46:05 +0000

    fix bug when you logged out on different ofr multiple browser

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@ class User < ApplicationRecord
   
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     Bcrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
