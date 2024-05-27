60ece25e7106130bbad66016e1ce3e977fa97097
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-05-27 00:37:01 +0000

    Enhance user authentication with session and cookie tokens

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,19 +3,25 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する
+    # 詳しくは https://techracho.bpsinc.jp/hachi8833/2023_06_02/130443 を参照
+    session[:session_token] = user.session_token
   end
 
-  # 永続的なセッションのためユーザーをDBに記憶する
+  # 永続的なセッションのためユーザーをデータベースに記憶する
   def remember(user)
     user.remember
     cookies.permanent.encrypted[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 記憶トークンcookieに対応するユーザーを返す
+  # 記憶トークンのcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token  # セッショントークンの比較
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
