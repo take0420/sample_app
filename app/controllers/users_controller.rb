51a4693eb2ebd625cee0ee4892d462026c44016b
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-05-30 00:57:20 +0000

    add update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find_by(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
 
     # Strong Parameters
