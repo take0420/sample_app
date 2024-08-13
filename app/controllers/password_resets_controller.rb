28fbf2c8f8e187c358da23b1594fa57455b8f676
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-08-13 13:45:16 +0000

    feat:

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action 'get_user,   only: [:edit, :update]'
+  before_action 'valid_user, only: [:edit, :update]'
+
   def new; end
 
   def create
@@ -15,4 +18,17 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit; end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # 正しいユーザーか確認する
+  def valid_user
+    return if @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+
+    redirect_to root_url
+  end
 end
