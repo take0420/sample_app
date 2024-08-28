de070c50fa03204e27a709969c8505ea40c10f15
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-08-28 01:01:16 +0000

    feat:

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,13 +1,14 @@
 class PasswordResetsController < ApplicationController
-  before_action 'get_user,   only: [:edit, :update]'
-  before_action 'valid_user, only: [:edit, :update]'
+  before_action :get_user,         only: %i[edit update]
+  before_action :valid_user,       only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update]
 
   def new; end
 
   def create
-    @user = User.find_by(email: params[:password_resets][:email].downcase)
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      @user.create_reset_digeset
+      @user.create_reset_digest
       @user.send_password_reset_email
       flash[:info] = 'Email sent with password reset instructions'
       redirect_to root_url
@@ -19,16 +20,45 @@ class PasswordResetsController < ApplicationController
 
   def edit; end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)
+      reset_session
+      log_in @user
+      flash[:success] = 'Password has been reset.'
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # beforeフィルタ
 
   def get_user
     @user = User.find_by(email: params[:email])
   end
 
-  # 正しいユーザーか確認する
+  # 有効なユーザーかどうか確認する
   def valid_user
-    return if @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+    unless @user && @user.activated? &&
+           @user.authenticated?(:reset, params[:id])
+      redirect_to root_url
+    end
+  end
 
-    redirect_to root_url
+  # トークンが期限切れかどうか確認する
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = 'Password reset has expired.'
+    redirect_to new_password_reset_url
   end
 end
