f07ee3ba2a5e9c4fca1524620fbbebec882c7654
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-09-18 00:49:59 +0000

    seed: micropost sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,17 @@ User.create!(name: 'Example User',
   name  = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
   password = 'password'
-  User.create!(name:  name,
-               email: email,
-               password:              password,
+  User.create!(name:,
+               email:,
+               password:,
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+
+  # ユーザーの一部を対象にマイクロポストを生成する
+  users = User.order(:created_at).take(6)
+  50.times do
+    content = Faker::Lorem.sentence(word_count: 5)
+    users.each { |user| user.microposts.create!(content:) }
+  end
 end
