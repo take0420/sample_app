734cfcc3c5f046f7ed29af6835c08e03f18e2f2f
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-10-14 03:52:24 +0000

    fix:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = 'Profile updated'
+      flash[:success] = 'Profile updated'   
       redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
