28f86c2c0982ce65dd0de1763d979dfef5e919ff
Takehiro_Yamazaki <111678214+take0420@users.noreply.github.com>
2024-10-15 02:02:58 +0000

    :update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[index edit update destroy]
-  before_action :correct_user,   only: %i[edit update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
   def index
@@ -16,13 +16,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
-      Rails.logger.debug "User created: #{@user.inspect}"
       @user.send_activation_email
-      flash[:info] = 'Please check your email to activate your account.'
+      flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
@@ -30,13 +28,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = 'Profile updated'
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
@@ -45,38 +41,27 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = 'User deleted'
+    flash[:success] = "User deleted"
     redirect_to users_url, status: :see_other
-  end
-
-  # アカウントを有効にする
-  def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
-
-  # 有効化用のメールを送信する
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
   end
 
   private
 
-  # Strong Parameters
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
-  # beforeフィルタ
+    # beforeフィルタ
 
-  # 正しいユーザーか確認
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url, status: :see_other) unless current_user?(@user)
-  end
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
 
-  def admin_user
-    redirect_to(root_url, status: :see_other) unless current_user.admin?
-  end
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
+    end
 end
